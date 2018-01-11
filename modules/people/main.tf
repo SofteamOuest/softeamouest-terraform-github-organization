a60cd537cf1dd87c93b512d638d85da78a02a3f1
victor.salaun <victor.salaun@gmail.com>
2018-01-11 23:31:19 +0100

    ajout de victorsalaun en tant qu'admin de l'organisation + invitation de KevinPradeau en tant que membre

---
+++ modules/people/main.tf
@@ -0,0 +1,9 @@
+resource "github_membership" "victorsalaun" {
+  username = "victorsalaun"
+  role = "admin"
+}
+
+resource "github_membership" "KevinPradeau" {
+  username = "KevinPradeau"
+  role = "member"
+}
