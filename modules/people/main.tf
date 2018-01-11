f345fd319e4606432bbd958ecb16584de10706b0
victor.salaun <victor.salaun@gmail.com>
2018-01-11 23:48:11 +0100

    add all current members

--- modules/people/main.tf
+++ modules/people/main.tf
@@ -7,3 +7,48 @@ resource "github_membership" "KevinPradeau" {
   username = "KevinPradeau"
   role = "member"
 }
+
+resource "github_membership" "alexthomazo" {
+  username = "alexthomazo"
+  role = "admin"
+}
+
+resource "github_membership" "apesme" {
+  username = "apesme"
+  role = "admin"
+}
+
+resource "github_membership" "elkouhen" {
+  username = "elkouhen"
+  role = "admin"
+}
+
+resource "github_membership" "Maeevick" {
+  username = "Maeevick"
+  role = "admin"
+}
+
+resource "github_membership" "ograton" {
+  username = "ograton"
+  role = "admin"
+}
+
+resource "github_membership" "rlopessilva" {
+  username = "rlopessilva"
+  role = "admin"
+}
+
+resource "github_membership" "slegalludec" {
+  username = "slegalludec"
+  role = "admin"
+}
+
+resource "github_membership" "thibaultgonce" {
+  username = "thibaultgonce"
+  role = "admin"
+}
+
+resource "github_membership" "yvzn" {
+  username = "yvzn"
+  role = "admin"
+}
